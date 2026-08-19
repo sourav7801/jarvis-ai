51ec36f0eb36bb00ad94bb945b269e9585d23527
sourav7801 <165499150+sourav7801@users.noreply.github.com>
2026-08-19 23:25:17 +0530

    Add V3.2 branch marker test

---
+++ tests/test_quant_v32_placeholder.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class QuantV32BranchMarkerTests(unittest.TestCase):
+    def test_marker(self):
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
